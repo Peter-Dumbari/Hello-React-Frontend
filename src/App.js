c82183ef78a01972df06fee5ef1936baf4fa9302
Peter Dumbari <dummytwiz@gmail.com>
2023-10-05 12:54:40 +0100

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Greeting from './Components/greeting';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Greeting />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
